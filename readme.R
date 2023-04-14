0da864b628144bb007333c63e6ea73bd5c306e75
Duccio Rocchini <ducciorocchini@gmail.com>
2023-04-14 12:10:08 +0200

    Update readme.R

--- readme.R
+++ readme.R
@@ -44,30 +44,78 @@ p2 <- ggplot() + geom_raster(testdp2, mapping = aes(x = x, y = y, fill = PC2)) +
 
 p1 + p2 
 
-#Classification
-library(raster)
+# Classification
+
+# data import
+so <- brick("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg")
+
+plotRGB(so, 1, 2, 3, stretch="lin")
+plotRGB(so, 1, 2, 3, stretch="hist")
+
+# Classifying the solar data
 
 # https://rspatial.org/raster/rs/4-unsupclassification.html
 
-# convert to matrix
-nr <- getValues(test)
-nr
+# 1. Get all the single values
+singlenr <- getValues(so)
+singlenr
+#
+# set.seed(99)
 
-set.seed(99)
-# kmncluster <- kmeans(na.omit(nr), centers = 3, iter.max = 500, nstart = 5, algorithm="Lloyd")
+# 2. Classify
+kcluster <- kmeans(singlenr, centers = 3)
+kcluster
 
-kmncluster <- kmeans(na.omit(nr), centers = 3)
-kmncluster
-str(kmncluster) # to see where the clusters are created
+# 3. Set values to a raster on the basis of so
+soclass <- setValues(so[[1]], kcluster$cluster) # assign new values to a raster object
 
-# Use the ndvi object to set the cluster values to a new raster
-knr <- setValues(test, kmncluster$cluster) # assign new values to a raster object
-# You can also do it like this
-knr <- raster(test)
-values(knr) <- kmncluster$cluster
-knr
+cl <- colorRampPalette(c('yellow','black','red'))(100)
+plot(soclass, col=cl)
 
-plot(knr)
+# set.seed can be used for repeating the experiment in the same manner for N times
+# http://rfunction.com/archives/62
+
+####
+
+# day 2 Grand Canyon
+
+gc <- brick("dolansprings_oli_2013088_canyon_lrg.jpg")
+gc
+
+# rosso = 1
+# verde = 2
+# blu = 3
+
+plotRGB(gc, r=1, g=2, b=3, stretch="lin")
+
+# change the stretch to histogram stretching
+plotRGB(gc, r=1, g=2, b=3, stretch="hist")
+
+# classification
+
+# 1. Get values
+singlenr <- getValues(gc)
+singlenr
+
+# 2. Classify
+kcluster <- kmeans(singlenr, centers = 3)
+kcluster
+
+# 3. Set values
+gcclass <- setValues(gc[[1]], kcluster$cluster) # assign new values to a raster object
+
+cl <- colorRampPalette(c('yellow','black','red'))(100)
+plot(gcclass, col=cl)
+
+frequencies <- freq(gcclass)
+tot = 58076148
+percentages = frequencies * 100 /  tot
+
+# Exercise: classify the map with 4 classes
+
+
+
+
 
 
 ##### Interesting functions of terra
